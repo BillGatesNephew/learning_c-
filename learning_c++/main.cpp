5507ede11e033fbf44d0f1fb61d594c6a8a486b7
Walker Willetts <wlkrwilletts@gmail.com>
2017-03-20 11:58:12 -0400

    Initial Commit

---
+++ learning_c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learning_c++
+//
+//  Created by Walker Willetts on 3/20/17.
+//  Copyright © 2017 W-squared. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
